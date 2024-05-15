9a70ae6dfe131f2ac3f168051316243254e522fa
szlwin <szlwin@163.com>
2024-05-15 13:52:53 +0800

    Update SimpleContainerListener.java

--- dec-demo/src/main/java/dec/demo/model/SimpleContainerListener.java
+++ dec-demo/src/main/java/dec/demo/model/SimpleContainerListener.java
@@ -3,12 +3,16 @@ package dec.demo.model;
 import dec.core.model.container.ResultInfo;
 import dec.core.model.container.listener.ContainerEvent;
 import dec.core.model.container.listener.ContainerListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SimpleContainerListener implements ContainerListener{
 
+	private final static Logger log = LoggerFactory.getLogger(SimpleContainerListener.class);
+	
 	@Override
 	public ResultInfo notify(ContainerEvent event) {
-		System.out.println("ContainerEvent:"+event.getType());
+		log.info("ContainerEvent:"+event.getType());
 		return ResultInfo.success();
 	}
 
